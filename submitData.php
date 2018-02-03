51535f2943c3ada8b9c6f1d4bddd1361b9e0bfdc
lspazzoli <lspazzoli@gmail.com>
2018-02-03 02:37:44 +0200

    Half of D done , Data Posted

--- submitData.php
+++ submitData.php
@@ -1,3 +1,6 @@
 <?php 
-	echo "Data Submitted";
+	$firstName=$_REQUEST["Fname"];
+	$lastName=$_REQUEST["Lname"];
+	$email=$_REQUEST["emailAddress"];
+	die ("<p style='margin-top: -95%;font-size:30px;'>Data Submitted:".$firstName."</p>");
 ?>
